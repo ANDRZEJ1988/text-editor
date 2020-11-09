0d60befa76aff510f7cce6bb129570b52e9587a1
ANDRZEJ1988 <Molotkov2010>
2020-11-09 14:33:56 +0200

    Updates

---
+++ precache-manifest.e56399923c46c2eddb4b04ef210d71a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fff326c868400eb88aaf9d110a1e8e",
+    "url": "/text-editor/index.html"
+  },
+  {
+    "revision": "8700bfdb767a22415c6f",
+    "url": "/text-editor/static/css/main.b47744b1.chunk.css"
+  },
+  {
+    "revision": "9b22a2ee3b730c38aae1",
+    "url": "/text-editor/static/js/2.fbbfe38b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/text-editor/static/js/2.fbbfe38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8700bfdb767a22415c6f",
+    "url": "/text-editor/static/js/main.975e7868.chunk.js"
+  },
+  {
+    "revision": "c04ecbcae6c6ae9b8265",
+    "url": "/text-editor/static/js/runtime-main.aadab63b.js"
+  }
+]);
